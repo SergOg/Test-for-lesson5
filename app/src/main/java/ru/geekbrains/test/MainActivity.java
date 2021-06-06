d67e5ebbff527da68462b8696f7524309db84fd5
SergeOg <gosha_006@mail.ru>
2021-06-06 18:16:15 +0300

    Test for lesson5

--- app/src/main/java/ru/geekbrains/test/MainActivity.java
+++ app/src/main/java/ru/geekbrains/test/MainActivity.java
@@ -1,8 +1,11 @@
 package ru.geekbrains.test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("calculator://open");
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(Intent.createChooser(browserIntent, null));
+            }
+        });
     }
 }
